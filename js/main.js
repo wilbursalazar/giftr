53c882f2cf270ddc3ba1063360fc46c06e948570
Wilbur Salazar <wilbursalazar@w.lan>
2026-01-11 13:42:03 -0800

    hadle queries and giphy API added and search rendering

--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@ const form = document.getElementById("searchForm");
 const input = document.getElementById("searchInput");
 const resultsGrid = document.getElementById("resultsGrid");
 const statusEl = document.getElementById("status");
+const GIPHY_API_KEY = "hbKRRknTnybZGCmqXqtDdfI140XSzdKg";
 
-console.log("we looking gud: JS Connected", form, input, resultsGrid);
+console.log("JS Connected", form, input, resultsGrid);
 
 function setStatus(message) {
   statusEl.textContent = message;
@@ -26,21 +27,56 @@ function renderGif(url, altText) {
   resultsGrid.appendChild(item);
 }
 
-form.addEventListener("submit", function (event) {
+function renderGifResults(gifs) {
+  for (const gif of gifs) {
+    const url = gif.images.fixed_width.url;
+    const altText = gif.title || "GIF result";
+    renderGif(url, altText);
+  }
+}
+
+function showNoResults(query) {
+  clearResults();
+  setStatus(`Nothing Matches: ${query}`);
+}
+
+
+async function searchGiphy(query) {
+  const url =
+    "https://api.giphy.com/v1/gifs/search" +
+    "?api_key=" + GIPHY_API_KEY +
+    "&q=" + encodeURIComponent(query) +
+    "&limit=10";
+
+  const response = await fetch(url);
+
+  const data = await response.json();
+  return data;
+}
+
+
+form.addEventListener("submit", async function (event) {
   event.preventDefault();
 
   const query = input.value.trim();
-  console.log("User searched:", query);
-
-  if (query === "") {
-    return;
-  }
+  if (query === "") return;
 
   clearResults();
-  setStatus(`Searching for: ${query}`);
+  setStatus("Loading...");
 
-  renderGif(
-    "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExeHhjZzRpc21mMG5zcXptaWl6czA5dDltbnUwbTZ5b3M0MmcxenBiayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/olAik8MhYOB9K/giphy.gif",
-    `Result for ${query}`
-  );
+  try {
+    const data = await searchGiphy(query);
+
+    const gifs = data.data; // array
+    if (!gifs || gifs.length === 0) {
+      showNoResults(query);
+      return;
+    }
+
+    setStatus(`Results for: ${query}`);
+    renderGifResults(gifs);
+  } catch (error) {
+    console.error(error);
+    setStatus("Something went wrong. Check the console.");
+  }
 });
