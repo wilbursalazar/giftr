36b268cad545793a166fbcc8742394aafb35c411
Wilbur Salazar <wilbursalazar@w.lan>
2026-01-11 13:06:31 -0800

    search and form submit functionality

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
-const form = document.getElementById("searchForm")
-const input = document.getElementById("searchInput")
-const resultsGrid = document.getElementById("resultsGrid")
+const form = document.getElementById("searchForm");
+const input = document.getElementById("searchInput");
+const resultsGrid = document.getElementById("resultsGrid");
+
+console.log("JS connected:", form, input, resultsGrid);
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const query = input.value.trim();
+  console.log("User searched:", query);
+
+  if (query === "") {
+    return;
+  }
+});
