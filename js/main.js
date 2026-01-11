987b534d84ba05c782627d35fd3316398b8fbcad
Wilbur Salazar <wilbursalazar@w.lan>
2026-01-11 13:25:07 -0800

    added a crucial gif test

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const input = document.getElementById("searchInput");
 const resultsGrid = document.getElementById("resultsGrid");
 const statusEl = document.getElementById("status");
 
-console.log("JS connected:", form, input, resultsGrid);
+console.log("we looking gud: JS Connected", form, input, resultsGrid);
 
 function setStatus(message) {
   statusEl.textContent = message;
@@ -11,6 +11,19 @@ function setStatus(message) {
 
 function clearResults() {
   resultsGrid.innerHTML = "";
+}
+
+function renderGif(url, altText) {
+  const item = document.createElement("div");
+  item.className = "grid-item";
+
+  const img = document.createElement("img");
+  img.src = url;
+  img.alt = altText;
+  img.loading = "lazy";
+
+  item.appendChild(img);
+  resultsGrid.appendChild(item);
 }
 
 form.addEventListener("submit", function (event) {
@@ -25,4 +38,9 @@ form.addEventListener("submit", function (event) {
 
   clearResults();
   setStatus(`Searching for: ${query}`);
+
+  renderGif(
+    "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExeHhjZzRpc21mMG5zcXptaWl6czA5dDltbnUwbTZ5b3M0MmcxenBiayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/olAik8MhYOB9K/giphy.gif",
+    `Result for ${query}`
+  );
 });
